9f0ccdcd46148443d0b873fad0aaffbe80fd1163
worknbuyconsumendie <a_chapala@mail.ru>
2025-07-22 05:05:04 +0300

    refactor: customize logic around _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { Footer } from '@/components/layout/Footer';
+import { Header } from '@/components/layout/Header';
+import '@/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
+import { Toaster } from 'sonner';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+      <Header />
+      <Component {...pageProps} />
+      <Toaster />
+      <Footer />
+    </ThemeProvider>
+  );
 }
